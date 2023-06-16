4d26ee269f6c5d19ad62184a7a786c6884b05e09
AnichoKraus <anichokraus3123@gmail.com>
2023-06-16 01:27:37 +0100

    index - foot:corr

--- src/components/index/Footer.js
+++ src/components/index/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
 	return (
 		<section className="flex px-6 ssm:px-10 bg-[#080808]">
-			<footer className="max-w-[1350px] mx-auto w-full py-[50px] flex justify-between items-center">
+			<footer className="max-w-[1350px] mx-auto w-full py-[50px] flex justify-between items-center gap-6 flex-wrap max-md:text-center max-md:flex-col">
 				<img src="/images/logo.png" alt="Logo" className="w-20 h-20" />
 				<p className="font-clash leading-none text-[26px]">© Asapbot.xyz All rights reserved.</p>
 			</footer>
